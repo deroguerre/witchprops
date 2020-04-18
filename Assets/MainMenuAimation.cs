d10c91b73d5040fef89212f85024fc42ddc081ab
gui <deroguerre@gmail.com>
2020-04-18 16:52:19 +0200

    update main menu

---
+++ Assets/MainMenuAimation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuAimation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float xParallaxPower = 0;
+    public float yParallaxPower = 0;
+    
+    private Vector3 initialPosition;
+    
+    void Start()
+    {
+        initialPosition = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.mousePosition.x;
+        float y = Input.mousePosition.y;
+
+        Vector3 newPosition = new Vector3(x * (0.0001f * xParallaxPower), y * (0.0001f * yParallaxPower), 0);
+        gameObject.transform.position = initialPosition + newPosition;
+        // _camTargetRb.position = Vector3.Lerp(transform.position, _camTargetRb.position, Time.deltaTime * 50);
+    }
+}
